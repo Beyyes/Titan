1a09c04d9889114a6f7b058c814a1808e20beffb
Gaofei Cao <cgf1993@foxmail.com>
2018-08-10 17:15:13 +0800

    move mysql setup process from Dockerfile to shell

--- titan.management/airflow/airflow-setup.sh
+++ titan.management/airflow/airflow-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-
+apt-get install mysql-server mysql-client libmysqlclient-dev
+pip install mysqlclient airflow[mysql,crypto,password]
 airflow initdb
 airflow webserver -p 10100
 airflow scheduler
