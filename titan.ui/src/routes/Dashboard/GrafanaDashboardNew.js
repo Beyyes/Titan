42ec7990bfc30cadec22c296dbe132abf936fa9a
Gaofei Cao <cgf1993@foxmail.com>
2018-08-24 10:54:30 +0800

    add grafana port support

--- titan.ui/src/routes/Dashboard/GrafanaDashboardNew.js
+++ titan.ui/src/routes/Dashboard/GrafanaDashboardNew.js
@@ -5,7 +5,7 @@ export default class GrafanaDashboardNewWrapper extends Component {
   render() {
     return (
       <Fragment>
-        <iframe src="http://spark-master:30061/dashboard/new" width="100%" height="100%" frameBorder="0" scrolling="no"></iframe>
+        <iframe src="http://spark-master:3000/dashboard/new" width="100%" height="100%" frameBorder="0" scrolling="no"></iframe>
       </Fragment>
     )
   }
