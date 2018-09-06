8d990cd915c9d59a9cd9e7a10346985ca3ebad3f
Gaofei Cao <cgf1993@foxmail.com>
2018-09-06 18:47:49 +0800

    move position of start.sh

--- titan.deployment/airflow/start.sh
+++ titan.deployment/airflow/start.sh
@@ -8,4 +8,3 @@ sudo cp airflow-webserver.service /usr/lib/systemd/system
 sudo cp airflow-scheduler.service /usr/lib/systemd/system
 sudo systemctl start airflow-webserver
 sudo systemctl start airflow-scheduler
-cd ~/airflow
