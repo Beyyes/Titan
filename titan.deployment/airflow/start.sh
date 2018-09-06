cf010b5562e554d6713696db53843c14cb1e3dd2
Gaofei Cao <cgf1993@foxmail.com>
2018-09-06 18:47:12 +0800

    move position of airflow

--- titan.deployment/airflow/start.sh
+++ titan.deployment/airflow/start.sh
@@ -3,9 +3,9 @@
 sudo service mysql restart
 sudo apt-get install libmysqlclient-dev -y
 sudo pip install mysqlclient
-cd ~/airflow
 sudo mkdir -p /usr/lib/systemd/system
 sudo cp airflow-webserver.service /usr/lib/systemd/system
 sudo cp airflow-scheduler.service /usr/lib/systemd/system
 sudo systemctl start airflow-webserver
 sudo systemctl start airflow-scheduler
+cd ~/airflow
