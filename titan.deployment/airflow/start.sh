542205ef522e9dd5aa1ab122febc8438930ca1ce
Gaofei Cao <cgf1993@foxmail.com>
2018-09-11 11:37:56 +0800

    test mysql script

--- titan.deployment/airflow/start.sh
+++ titan.deployment/airflow/start.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
+sudo su
+chmod u+w /etc/mysql/mysql.conf.d/mysqld.cnf
+echo'explicit_defaults_for_timestamp = true'> /etc/mysql/mysql.conf.d/mysqld.cnf
+chmod u-w /etc/mysql/mysql.conf.d/mysqld.cnf
+exit
+
 sudo service mysql restart
 sudo apt-get install libmysqlclient-dev -y
 sudo pip install mysqlclient
+
+mysql -u root -p
+show databases
+create database airflow
+
 sudo mkdir -p /usr/lib/systemd/system
 sudo cp airflow-webserver.service /usr/lib/systemd/system
 sudo cp airflow-scheduler.service /usr/lib/systemd/system
