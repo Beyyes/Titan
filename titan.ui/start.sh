54a3e099abf0bf04120813f61628801abb823208
Gaofei Cao <cgf1993@foxmail.com>
2018-09-11 10:18:43 +0800

    ln node script to usrlocal

--- titan.ui/start.sh
+++ titan.ui/start.sh
@@ -4,5 +4,6 @@ wget https://nodejs.org/dist/v6.9.2/node-v6.9.2-linux-x64.tar.xz
 xz -d node-v6.9.2-linux-x64.tar.xz
 tar -xvf node-v6.9.2-linux-x64.tar
 
+sudo ln node-v6.9.2-linux-x64/bin/node /usr/local/bin
 sudo node-v6.9.2-linux-x64/bin/npm install
 sudo nohup node-v6.9.2-linux-x64/bin/npm start >out.file 2>&1 &
