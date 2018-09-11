629f4b99be08b450de6e9c06b88aefe9a56e6489
Gaofei Cao <cgf1993@foxmail.com>
2018-09-11 16:25:47 +0800

    add datastorage delete

--- titan.deployment/management.py
+++ titan.deployment/management.py
@@ -216,6 +216,7 @@ class Management:
             k8s_clean_cmd = "sudo systemctl stop kubelet && sudo systemctl stop docker && sudo rm -rf /var/lib/cni/ && sudo rm -rf /var/lib/kubelet/* && " \
                             "sudo rm -rf /etc/cni/ && sudo rm -rf /etc/kubernetes && sudo rm -rf /var/lib/etcd && sudo systemctl start kubelet && sudo systemctl start docker"
             deployment.remoteTool.execute_cmd(host, k8s_clean_cmd)
+            deployment.remoteTool.execute_cmd(host, "sudo rm -rf /datastorage")
         print("\r\n Delete node successfully!")
 
     def all_deploy(self):
